3bc91f9caff39e728bf0e4eabd5a0560407c9b17
Mario Lucassen <mario.lucassen@hotmail.com>
2017-12-23 13:43:58 +0100

    Moved Navigator field to A ControllerBase class

--- TheAnimeFetcher/Classes/Controllers/ControllerBase.cs
+++ TheAnimeFetcher/Classes/Controllers/ControllerBase.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheAnimeFetcher.Classes.Helpers;
 
 namespace TheAnimeFetcher.Classes.Controllers
 {
-    public class HomeController : ControllerBase
+    public abstract class ControllerBase
     {
-        
+        protected static Navigator Navigator = Navigator.Instance;
     }
 }
--- TheAnimeFetcher/Classes/Controllers/LoginController.cs
+++ TheAnimeFetcher/Classes/Controllers/LoginController.cs
@@ -4,16 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TheAnimeFetcher.Classes.Authentication;
-using TheAnimeFetcher.Classes.Helpers;
 using TheAnimeFetcher.Classes.Services;
 using TheAnimeFetcher.Classes.XML;
 using TheAnimeFetcher.Views;
 
 namespace TheAnimeFetcher.Classes.Controllers
 {
-    public static class LoginController
+    public class LoginController : ControllerBase
     {
-        private static Navigator Navigator = Navigator.Instance;
         public static async Task<bool> LoginAsync(Credentials credentials)
         {
             User User = await MAL.VerifyCredentials(credentials);
